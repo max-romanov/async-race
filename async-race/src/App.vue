267f82cf6f4c907ec1783d5dd042ecbcf11115f5
max-romanov <max.romanov.90@mail.ru>
2023-01-14 11:08:39 +0200

    feat: add base

---
+++ async-race/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+console.log('Hello World')
+</script>
+
+<template>
+  <main class="bg-blue-500">
+    <h1>Hello World</h1>
+  </main>
+</template>
+
+<style>
+main {
+  width: 100%;
+}
+</style>
